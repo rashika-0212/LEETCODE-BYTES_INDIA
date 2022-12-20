7a98791a8891ca8f8e99b6a726e39ce974913ccb
RASHIKA K V <107302130+rashika-0212@users.noreply.github.com>
2022-12-20 08:28:59 +0530

    Create SEARCH IN ROTATED SORTED ARRAY.java

---
+++ SEARCH IN ROTATED SORTED ARRAY.java
@@ -0,0 +1,24 @@
+int search(int* nums, int numsSize, int target){
+    int start = 0, end = numsSize - 1;
+    while(start <= end)
+    {
+        int mid = (start + end)/2;
+        if(target == nums[mid])
+            return mid;
+        else if(nums[start] <= nums[mid])
+        {
+            if(nums[start] <= target && target <= nums[mid])
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        else
+        {
+            if(nums[mid] <= target && target <= nums[end])
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+    }
+    return -1;
+}
